abbda0c1408d0d5ae12804bc14449f0df73308f3
Noah Hendrickson <noahjames017@gmail.com>
2022-03-21 20:46:00 -0400

    fix scroll bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import Navbar from "./Navbar";
 import Body from "./Body";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 function App() {
   return (
